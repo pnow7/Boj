9dd937727cb9f8ec0f648b64a9cc58d72c27f337
guswo4324 <phj2394324@naver.com>
2025-04-09 01:56:43 +0900

    [Bronze II] Title: 상수, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int A = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+		int B = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+		
+		if(A > B) {
+			System.out.println(A);
+		}
+		else {
+			System.out.println(B);
+		}
+		br.close();
+		
+	}
+
+}
