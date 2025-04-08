758f8ebb33b80ffc64854a5f83984021b7b159bf
guswo4324 <phj2394324@naver.com>
2025-04-09 02:15:17 +0900

    [Bronze I] Title: 이항 계수 1, Time: 108 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		int sum = 1;
+		for(int i = 0; i < K; i++) {
+			sum = sum * N;
+			N--;
+		}
+		
+		bw.write(sum/factorial(K)+"");
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+	
+	public static int factorial(int num) {
+		if(num == 0 || num == 1) {
+			return 1;
+		}
+		return num * factorial(num-1);
+	}
+
+}
