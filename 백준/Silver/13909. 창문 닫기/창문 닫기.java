0bf5d17006a4f45a3bdc030c66c7e20fdcfac421
guswo4324 <phj2394324@naver.com>
2025-04-09 02:12:14 +0900

    [Silver V] Title: 창문 닫기, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		bw.write((int)(Math.sqrt(N))+"");
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+
+}
