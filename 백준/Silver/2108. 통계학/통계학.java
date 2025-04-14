6a9e1e87e642e8be3c509ab40d061f510c3874b1
JJang9 <phj2394324@naver.com>
2025-04-14 18:59:08 +0900

    [Silver II] Title: 통계학, Time: 472 ms, Memory: 48432 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.java
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -4,55 +4,53 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         int N = Integer.parseInt(br.readLine());
-
-        List<Integer> numbers = new ArrayList<>();
-        Map<Integer, Integer> freqMap = new HashMap<>();
+        int[] nums = new int[N];
+        int[] count = new int[8001]; // -4000 ~ 4000 -> 0 ~ 8000
         int sum = 0;
 
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
 
         for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(br.readLine());
-            numbers.add(num);
-            sum += num;
+            int val = Integer.parseInt(br.readLine());
+            nums[i] = val;
+            sum += val;
+            count[val + 4000]++;
 
-            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
-
-            max = Math.max(max, num);
-            min = Math.min(min, num);
+            if (val > max) max = val;
+            if (val < min) min = val;
         }
 
-        // 정렬
-        Collections.sort(numbers);
+        Arrays.sort(nums); // 중앙값 구하기 위해 정렬
 
-        // 산술 평균
-        int avg = (int) Math.round((double) sum / N);
-
-        // 중앙값
-        int median = numbers.get(N / 2);
-
-        // 최빈값 찾기
-        int maxFreq = Collections.max(freqMap.values());
-
-        List<Integer> modes = new ArrayList<>();
-        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
-            if (entry.getValue() == maxFreq) {
-                modes.add(entry.getKey());
+        // 최빈값 구하기
+        int modeMax = 0;
+        for (int i = 0; i < 8001; i++) {
+            if (count[i] > modeMax) {
+                modeMax = count[i];
             }
         }
 
-        Collections.sort(modes); // 최빈값 여러 개일 때 작은 순 정렬
-        int mode = (modes.size() > 1) ? modes.get(1) : modes.get(0);
-
-        // 범위
-        int range = max - min;
+        int mode = 0;
+        boolean first = true;
+        for (int i = 0; i < 8001; i++) {
+            if (count[i] == modeMax) {
+                if (first) {
+                    mode = i - 4000;
+                    first = false;
+                } else {
+                    mode = i - 4000;
+                    break;
+                }
+            }
+        }
 
         // 결과 출력
-        System.out.println(avg);
-        System.out.println(median);
-        System.out.println(mode);
-        System.out.println(range);
+        System.out.println(Math.round((double) sum / N)); // 산술 평균
+        System.out.println(nums[N / 2]);                 // 중앙값
+        System.out.println(mode);                        // 최빈값
+        System.out.println(max - min);                   // 범위
     }
 }
