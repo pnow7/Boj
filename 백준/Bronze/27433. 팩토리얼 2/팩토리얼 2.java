d8fc5857c613ed4b5ddef7711599fab08dac6db3
JJang9 <phj2394324@naver.com>
2025-04-17 22:07:12 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 104 ms, Memory: 14376 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.math.BigInteger;
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		System.out.println(factorial(N));
+		br.close();
+	}
+	
+	public static BigInteger factorial(int num) {
+		if(num == 0 || num == 1) return BigInteger.ONE;
+
+		return new BigInteger(""+num).multiply(factorial(num-1));
+	
+
+	}
+}
