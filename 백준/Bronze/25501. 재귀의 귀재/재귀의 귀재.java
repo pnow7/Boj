a0dcafd40e02ac9bba7a303ffe7490ed7c1b641d
JJang9 <phj2394324@naver.com>
2025-04-19 23:25:24 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 236 ms, Memory: 19684 KB -BaekjoonHub

--- 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
@@ -8,31 +8,23 @@ public class Main {
 		int T = Integer.parseInt(br.readLine());
 		for(int i = 0; i < T; i++) {
 			String s = br.readLine();
-			sb.append(isPalindrome(s)+" "+isPalindromeCount(s)+"\n");
+			sb.append(isPalindrome(s)+" "+count+"\n");
 		}
 		System.out.print(sb);
 		br.close();
 	}
 	
 	public static int recursion(String s, int l, int r) {
+		count++;
 		if(l >= r) return 1;
 		else if(s.charAt(l) != s.charAt(r)) return 0;
 		else return recursion(s, l+1, r-1);
 	}
 	
 	public static int isPalindrome(String s) {
+		count = 0;
 		return recursion(s, 0, s.length()-1);
 	}
 	
-	public static int recursionCount(String s, int l, int r) {
-		count++;
-		if(l >= r) return count;
-		else if(s.charAt(l) != s.charAt(r)) return count;
-		else return recursionCount(s, l+1, r-1);	
-	}
-	public static int isPalindromeCount(String s) {
-		count = 0;
-		return recursionCount(s, 0, s.length()-1);
-	}
 	
 }
