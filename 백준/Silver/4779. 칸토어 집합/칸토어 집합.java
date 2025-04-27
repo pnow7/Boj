0d4c20614d53b2983ddb30159d829fa1c923a91a
현재 <phj2394324@naver.com>
2025-04-27 21:43:58 +0900

    [Silver III] Title: 칸토어 집합, Time: 212 ms, Memory: 19556 KB -BaekjoonHub

---
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String num;
+        while((num = br.readLine()) != null) {
+            int N = Integer.parseInt(num);
+            System.out.println(func(N));
+        }
+    }
+
+    public static String func(int n) {
+        if (n == 0) return "-";
+        String str = func(n - 1);
+        return str + " ".repeat(str.length()) + str;
+    }
+}
