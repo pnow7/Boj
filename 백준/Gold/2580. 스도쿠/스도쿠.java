b521849f8d15cd045f09850f38b4172f23bc6bbf
현재 <phj2394324@naver.com>
2025-06-06 17:01:11 +0900

    [Gold IV] Title: 스도쿠, Time: 424 ms, Memory: 22356 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -1,44 +1,48 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[][] board = new int[9][9];
-    static ArrayList<int[]> blanks = new ArrayList<>();
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                board[i][j] = sc.nextInt();
-                if (board[i][j] == 0) {
-                    blanks.add(new int[]{i, j});
-                }
-            }
-        }
-
-        solve(0);
-    }
-
-    public static void solve(int idx) {
-        if (idx == blanks.size()) {
-            printBoard();
-            System.exit(0); 
-        }
-
-        int[] pos = blanks.get(idx);
-        int x = pos[0];
-        int y = pos[1];
-
-        for (int num = 1; num <= 9; num++) {
-            if (isValid(x, y, num)) {
-                board[x][y] = num;
-                solve(idx + 1);
-                board[x][y] = 0; 
-            }
-        }
-    }
-
-    public static boolean isValid(int x, int y, int num) {
+	static int[][] board = new int[9][9];
+	static ArrayList<int[]> blanks = new ArrayList<>();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		for(int i = 0; i < 9; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < 9; j++) {
+				board[i][j] = Integer.parseInt(st.nextToken());
+				
+				if(board[i][j] == 0) {
+					blanks.add(new int[] {i, j});
+				}
+			}
+		}
+		br.close();
+		solve(0);
+	}
+	
+	public static void solve(int idx) {
+		if(idx == blanks.size()) {
+			printBoard();
+			System.exit(0); 
+		}
+		
+		int[] pos = blanks.get(idx);
+		int x = pos[0];
+		int y = pos[1];
+		
+		for(int num = 1; num <= 9; num++) {
+			if(isValid(x, y, num)) {
+				board[x][y] = num;
+				solve(idx + 1);
+				board[x][y] = 0;
+			}
+		}
+	}
+	
+	public static boolean isValid(int x, int y, int num) {
         for (int i = 0; i < 9; i++) {
             if (board[x][i] == num) return false;
         }
@@ -56,8 +60,8 @@ public class Main {
         }
 
         return true;
-    }
-
+	}
+	
     public static void printBoard() {
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
