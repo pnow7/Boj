83abe678da801f2eb0cee4908fd8d63f063d3e13
현재 <phj2394324@naver.com>
2025-06-20 00:16:23 +0900

    [Gold V] Title: 전깃줄, Time: 108 ms, Memory: 14392 KB -BaekjoonHub

---
+++ 백준/Gold/2565. 전깃줄/전깃줄.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+
+        int[] A = new int[N];
+        int[] B = new int[N];
+
+        int[][] wires = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            wires[i][0] = Integer.parseInt(st.nextToken()); // A
+            wires[i][1] = Integer.parseInt(st.nextToken()); // B
+        }
+        
+        Arrays.sort(wires, Comparator.comparingInt(o -> o[0]));
+
+        int[] dp = new int[N];
+        Arrays.fill(dp, 1);
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (wires[j][1] < wires[i][1]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(N - max);
+    }
+}
