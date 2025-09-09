dced9e5f40a7fe0eb82947b00d2867fcb0d6b5b8
현재 <phj2394324@naver.com>
2025-09-10 00:54:23 +0900

    [Silver V] Title: 색종이, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,12 +1,20 @@
 import sys
 
 n = int(sys.stdin.readline())
-covered = set()
+arr = [ [False] * 100 for _ in range(100) ]
 
 for _ in range(n):
     x, y = map(int, sys.stdin.readline().split())
+
     for i in range(x, x + 10):
         for j in range(y, y + 10):
-            covered.add((i, j))
+            arr[i][j] = True
 
-print(len(covered))
+count = 0
+
+for i in range(100):
+    for j in range(100):
+        if arr[i][j] == True:
+            count += 1
+
+print(count)
