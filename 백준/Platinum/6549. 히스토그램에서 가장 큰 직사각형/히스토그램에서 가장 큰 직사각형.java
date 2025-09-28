cb9b580fbc493b6e11d8865636fc6a4c7cb14993
현재 <phyunjae7333@gmail.com>
2025-09-28 18:40:35 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 444 ms, Memory: 46948 KB -BaekjoonHub

---
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            
+            if (n == 0) break;
+            
+            int[] heights = new int[n];
+            for (int i = 0; i < n; i++) {
+                heights[i] = Integer.parseInt(st.nextToken());
+            }
+            
+            sb.append(findMaxArea(heights, 0, n - 1)).append("\n");
+        }
+        
+        System.out.print(sb);
+        br.close();
+    }
+    
+    static long findMaxArea(int[] heights, int start, int end) {
+        if (start == end) {
+            return (long) heights[start];
+        }
+        
+        int mid = (start + end) / 2;
+        int left = mid;
+        int right = mid + 1;
+        
+        long centerArea = 0;
+        long leftArea = findMaxArea(heights, start, mid);
+        long rightArea = findMaxArea(heights, mid + 1, end);
+        long h = Math.min(heights[left], heights[right]);
+        
+        centerArea = Math.max(leftArea, rightArea);
+        centerArea = Math.max(centerArea, h * 2);
+        
+        while (start < left || right < end) {
+            if (right == end || (start < left && heights[left - 1] >= heights[right + 1])) {
+                left--;
+                h = Math.min(h, heights[left]);
+            } else {
+                right++;
+                h = Math.min(h, heights[right]);
+            }
+            
+            centerArea = Math.max(centerArea, h * (right - left + 1));
+        }
+        
+        return Math.max(Math.max(leftArea, rightArea), centerArea);
+    }
+    
+}
