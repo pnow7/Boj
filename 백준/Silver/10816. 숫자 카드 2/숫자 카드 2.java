923530b6c8aa3734f3723f3f535d8421e120ebad
현재 <phyunjae7333@gmail.com>
2025-10-02 00:53:53 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1340 ms, Memory: 121880 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,67 +1,67 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-		
-		int N = Integer.parseInt(br.readLine());
-		int[] num = new int[N];
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < N; i++) {
-			num[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		Arrays.sort(num);
-		
-		int M = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
 
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < M; i++) {
-			int target = Integer.parseInt(st.nextToken());
-			sb.append(upperBound(num, target) - lowerBound(num, target)).append(' ');
-		}
-		bw.write(sb+" ");
-		br.close();
-		bw.flush();
-		bw.close();
-			
-	}
-	public static int lowerBound(int[] arr, int key) {
-		int lo = 0;
-		int hi = arr.length;
-		
-		while(lo < hi) {
-			int mid = (lo+hi)/2;
-			
-			if(key <= arr[mid]) {
-				hi = mid;
-			}
-			else {
-				lo = mid + 1;
-			}
-		}
-		return lo;
-	}
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-	public static int upperBound(int[] arr, int key) {
-		int lo = 0;
-		int hi = arr.length;
-		
-		while(lo < hi) {
-			int mid = (lo+hi)/2;
-			
-			if(key < arr[mid]) {
-				hi = mid;
-			}
-			else {
-				lo = mid + 1;
-			}
-		}
-		return lo;
-	}
+        Arrays.sort(arr);
+
+        int M = Integer.parseInt(br.readLine());
+        
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            sb.append(upperBound(arr, num)  - lowerBound(arr, num)).append(" ");
+        }
+
+        System.out.print(sb.toString());
+        br.close();
+        
+    }
+
+    static int lowerBound(int[] arr, int target) {
+        int lo = 0;
+        int hi = arr.length;
+
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            if (arr[mid] >= target) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+
+        return lo;
+    }
+
+    static int upperBound(int[] arr, int target) {
+        int lo = 0;
+        int hi = arr.length;
+
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            if (arr[mid] > target) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+
+        return lo;
+    }
+
 }
-
