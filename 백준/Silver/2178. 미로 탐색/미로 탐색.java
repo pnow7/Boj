b65d86a641dba43afde9e784c451b3c7db6c07d5
현재 <phyunjae7333@gmail.com>
2025-10-21 19:44:28 +0900

    [Silver I] Title: 미로 탐색, Time: 132 ms, Memory: 15256 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    static int N, M;
+    static boolean[][] visited;
+    static int[][] miro;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    
+    static class Node {
+        int x, y;
+        
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+        visited = new boolean[N][M];
+        miro = new int[N][M];
+        
+        for (int i = 0; i < N; i++) {
+            String strNum = br.readLine();
+            for (int j = 0; j < M; j++) {
+                String charToString = String.valueOf(strNum.charAt(j));
+                miro[i][j] = Integer.parseInt(charToString);
+            }
+        }
+        
+        BFS(0, 0);
+        
+        System.out.println(miro[N - 1][M - 1]);
+        br.close();
+        
+    }
+    
+    static void BFS(int x, int y) {
+        Queue<Node> q = new ArrayDeque<>();
+        q.offer(new Node(x, y));
+        visited[x][y] = true;
+        miro[x][y] = 1;
+
+        while (!q.isEmpty()) {
+            Node currentNode = q.poll();            
+            
+            for (int i = 0; i < 4; i++) {
+                int nextX = currentNode.x + dx[i];
+                int nextY = currentNode.y + dy[i];
+                
+                if (nextX < 0 || nextX >= N || nextY < 0 || nextY >= M) {
+                    continue;
+                }
+                
+                if (miro[nextX][nextY] == 0) {
+                    continue;
+                }
+                
+                if (visited[nextX][nextY] == true) {
+                    continue;
+                }
+                
+                q.offer(new Node(nextX, nextY));
+                visited[nextX][nextY] = true;
+                
+                miro[nextX][nextY] = miro[currentNode.x][currentNode.y] + 1;
+            }
+        }
+    }
+    
+}
